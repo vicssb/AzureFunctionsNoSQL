35e5951ad573a219ac9474d195546a3207fefd66
Victor <vicssb@gmail.com>
2025-01-13 14:54:45 -0300

    Created fnGetMoviesDetail commit.

---
+++ fnGetMoviesDetail/MovieResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fnGetMoviesDetail
+{
+    internal class MovieResult
+    {
+            public string Id { get; set; }
+            public string Title { get; set; }
+            public string Year { get; set; }
+            public string Video { get; set; }
+            public string Thumb { get; set; }
+    }
+}
